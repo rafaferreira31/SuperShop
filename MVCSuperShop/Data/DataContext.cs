f20cfd200115c669caac472a8d600a28a4b01a5a
rafaferreira31 <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-08-26 18:49:31 +0100

    First Controller

---
+++ MVCSuperShop/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCSuperShop.Data.Entities;
+
+namespace MVCSuperShop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- MVCSuperShop/Program.cs
+++ MVCSuperShop/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MVCSuperShop.Data;
+
 namespace MVCSuperShop
 {
     public class Program
@@ -8,6 +11,11 @@ namespace MVCSuperShop
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
