1da213f435cf6d48661447b70c849516b2526e3f
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-09-19 00:12:39 +0100

    Products Combo

--- MVCSuperShop/Data/IProductRepository.cs
+++ MVCSuperShop/Data/IProductRepository.cs
@@ -1,10 +1,14 @@
 ﻿using SuperShop.Data.Entities;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SuperShop.Data
 {
     public interface IProductRepository : IGenericRepository<Product>
     {
         public IQueryable GetAllWithUsers();
+
+        IEnumerable<SelectListItem> GetComboProducts();
     }
 }
