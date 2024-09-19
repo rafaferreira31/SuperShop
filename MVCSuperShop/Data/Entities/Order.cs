4efcc2b2b0501bb51a69ce961739649afae3cbbd
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-09-19 10:55:28 +0100

    Confirm Orders

--- MVCSuperShop/Data/Entities/Order.cs
+++ MVCSuperShop/Data/Entities/Order.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.VisualBasic;
 
 
@@ -33,11 +34,20 @@ namespace SuperShop.Data.Entities
         public IEnumerable<OrderDetail> Items { get; set; }
 
 
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Lines => Items == null ? 0 : Items.Count();
+
+
         [DisplayFormat(DataFormatString = "{0:N2}")]
         public double Quantity => Items == null ? 0 : Items.Sum(i => i.Quantity);
 
 
         [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal Value => Items == null ? 0 : Items.Sum(i => i.Value);
+
+
+        [Display(Name = "Order Date")]
+        [DisplayFormat(DataFormatString = "{0:mm/dd/yyyy HH:mm}", ApplyFormatInEditMode = false)]
+        public DateTime? OrderDateLocal => this.OrderDate == null ? null : this.OrderDate.ToLocalTime();
     }
 }
